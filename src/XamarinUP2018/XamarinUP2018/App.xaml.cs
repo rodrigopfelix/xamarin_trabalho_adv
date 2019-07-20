b1a41df225ec3b471bfd0b27bfc212a9d60fbff8
William <Rodriguez>
2019-07-19 22:14:42 -0300

    prism 101

--- src/XamarinUP2018/XamarinUP2018/App.xaml.cs
+++ src/XamarinUP2018/XamarinUP2018/App.xaml.cs
@@ -1,18 +1,33 @@
-﻿using System;
+﻿using Prism;
+using Prism.DryIoc;
+using Prism.Ioc;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XamarinUP2018.ViewModels;
+using XamarinUP2018.Views;
 
 namespace XamarinUP2018
 {
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
-        public App()
+        public App() : this(null) { }
+
+        public App(IPlatformInitializer initializer) : base(initializer) { }
+
+        protected override async void OnInitialized()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            await NavigationService.NavigateAsync($"{nameof(NavigationPage)}/{nameof(HomePage)}");
         }
 
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>(nameof(NavigationPage));
+            containerRegistry.RegisterForNavigation<HomePage, HomeViewModel>(nameof(HomePage));
+        }
+        
         protected override void OnStart()
         {
             // Handle when your app starts
